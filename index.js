ec726b5ff68c22b21bb2689b6524148860751655
adityarizkyramadhan <90701908+adityarizkyramadhan@users.noreply.github.com>
2023-08-30 20:07:04 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,64 @@
+// Buat fungsi penambahan
+function tambah(a, b) {
+    return a + b;
+}
+
+// Buat fungsi pengurangan
+function kurang(a, b) {
+    return a - b;
+}
+
+// Buat fungsi perkalian
+function kali(a, b) {
+    return a * b;
+}
+
+// Buat fungsi pembagian
+function bagi(a, b) {
+    return a / b;
+}
+
+// Buat fungsi pangkat
+function pangkat(a, b) {
+    return a ** b;
+}
+
+// Buat fungsi akar
+function akar(a) {
+    return Math.sqrt(a);
+}
+
+// Buat fungsi persen
+function persen(a) {
+    return a / 100 + '%';
+}
+
+// Buat fungsi pi
+function pi() {
+    return Math.PI;
+}
+
+// Buat fungsi e
+function e() {
+    return Math.E;
+}
+
+// Buat fungsi logaritma
+function log(a) {
+    return Math.log10(a);
+}
+
+// Buat fungsi logaritma natural
+function ln(a) {
+    return Math.log(a);
+}
+
+// Buat fungsi sin
+function sin(a) {
+    return Math.sin(a);
+}
+
+// Buat fungsi cos
+function cos(a) {
+    return Math.cos(a);
+}
